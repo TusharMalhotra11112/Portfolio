4298c1eff2d8b3857fd1bb745b04b72f1f520445
Tushar Malhotra <malhotratushar999@gmail.com>
2023-09-11 23:22:28 +0530

    live chat app

--- src/Components/projectData.js
+++ src/Components/projectData.js
@@ -1,6 +1,15 @@
 const data = [
     {
         id:1,
+        img: 'livechatapp.png',
+        alt:"livechatappImg",
+        description:"A live chat app facilitates real-time text-based communication between businesses and customers. It's a valuable tool for providing instant support, answering inquiries, and enhancing user engagement. These apps often include features like chatbots and conversation history tracking for efficiency and convenience.",
+        title:"Live Chat Application",
+        url:"https://tusharmalhotra-livechatapp.vercel.app",
+        status:"Online",
+    },
+    {
+        id:2,
         img: 'portfolio.png',
         alt:"PortfolioImg",
         description:"A portfolio is a collection of work or investments held by an individual or entity. It showcases their skills, experience, or financial holdings. Portfolios serve as a representation of accomplishments and can be used for various purposes, such as job applications or investment management.",
@@ -9,7 +18,7 @@ const data = [
         status:"Online",
     },
     {
-        id:2,
+        id:3,
         img:'TICTACTOEIMG.png',
         alt:"Tic Tac Toe Image",
         description:"Tic Tac Toe is a classic game played on a grid of 3x3 squares. Two players take turns marking X or O in empty squares with the objective of getting three of their symbols in a row, either horizontally, vertically, or diagonally. The game ends in a draw if all squares are filled without a winner.",
