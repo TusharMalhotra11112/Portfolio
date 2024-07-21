60b536b2ddd28783d2175ed99d3a569bf24ebe50
Tushar Malhotra <malhotratushar999@gmail.com>
2024-07-21 14:44:02 +0530

    data visualization

--- src/Components/projectData.js
+++ src/Components/projectData.js
@@ -1,6 +1,15 @@
 const data = [
     {
         id:1,
+        img:'Data_Visualization.png',
+        alt:"Data Visualization",
+        description:"Developed using the MERN stack (MongoDB, Express.js, React.js, Node.js) with Chart.js for visualization, the Data Visualization App offers real-time data integration, allowing users to seamlessly add, update, and filter datasets with dynamic, live-updating visualizations. It features interactive charts, customizable visual elements, and responsive filtering for an enhanced data exploration experience.",
+        title:"Data Visualization",
+        url:'https://tusharmalhotra-datavisualization.vercel.app',
+        status:"Completed",
+    },
+    {
+        id:2,
         img: 'livechatapp.png',
         alt:"livechatappImg",
         description:"A live chat app facilitates real-time text-based communication between businesses and customers. It's a valuable tool for providing instant support, answering inquiries, and enhancing user engagement. These apps often include features like chatbots and conversation history tracking for efficiency and convenience.",
@@ -9,7 +18,7 @@ const data = [
         status:"Online",
     },
     {
-        id:2,
+        id:3,
         img: 'portfolio.png',
         alt:"PortfolioImg",
         description:"A portfolio is a collection of work or investments held by an individual or entity. It showcases their skills, experience, or financial holdings. Portfolios serve as a representation of accomplishments and can be used for various purposes, such as job applications or investment management.",
@@ -18,7 +27,7 @@ const data = [
         status:"Online",
     },
     {
-        id:3,
+        id:4,
         img: 'dashboard.png',
         alt:"Dashboard",
         description:"IT is a dashboard that can be used to display my front-end skills.",
@@ -27,7 +36,7 @@ const data = [
         status:"Online",
     },
     {
-        id:4,
+        id:5,
         img:'github-repo.png',
         alt:"Github Repo display",
         description:"This application can be used to display public repositories with paginations",
@@ -36,7 +45,7 @@ const data = [
         status:"Completed",
     },
     {
-        id:5,
+        id:6,
         img:'TICTACTOEIMG.png',
         alt:"Tic Tac Toe Image",
         description:"Tic Tac Toe is a classic game played on a grid of 3x3 squares. Two players take turns marking X or O in empty squares with the objective of getting three of their symbols in a row, either horizontally, vertically, or diagonally. The game ends in a draw if all squares are filled without a winner.",
